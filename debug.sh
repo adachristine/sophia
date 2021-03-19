583d86019e24036794f39bdd8b64fa4cb990703c
Ada Christine <adachristine18@gmail.com>
2021-03-19 17:00:25 +0000

    debug output from serial to file

--- debug.sh
+++ debug.sh
@@ -8,5 +8,5 @@ make -C kernel &&
 qemu-system-x86_64 -cpu qemu64 \
     -drive if=pflash,format=raw,unit=0,file=/usr/share/ovmf/x64/OVMF_CODE.fd,readonly=on \
     -drive if=pflash,format=raw,unit=1,file=OVMF_VARS.fd \
-    -net none -drive file=uefi.img,if=ide -monitor stdio -vga none 
+    -net none -drive file=uefi.img,if=ide -monitor stdio -serial file:sophia.log 
 
