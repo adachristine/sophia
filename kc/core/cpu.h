7b45eb843e968b787b1ec367b41c98585227dbcd
Ada Christine <adachristine18@gmail.com>
2022-01-13 14:23:20 +0000

    msr_read() and msr_write(), rudimentary syscall thing

--- kc/core/cpu.h
+++ kc/core/cpu.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 void cpu_init(void);
 
 void isr_install(int vec, void (*isr)(void), int ist, int trap);
@@ -7,3 +9,7 @@ void ist_install(int ist, void *stack);
 
 void int_enable(void);
 void int_disable(void);
+
+uint64_t msr_read(uint32_t index);
+void msr_write(uint32_t index, uint64_t value);
+
