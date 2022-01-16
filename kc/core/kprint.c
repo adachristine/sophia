069a9bfecc4161a1a08b5116a5baf3189d8c0efa
Ada Christine <adachristine18@gmail.com>
2022-01-16 03:16:37 +0000

    selective optimization

--- kc/core/kprint.c
+++ kc/core/kprint.c
@@ -1,11 +1,15 @@
 #include "kprint.h"
 #include "serial.h"
 
+#include <stdarg.h>
+
+__attribute__((optimize("O3")))
 int kputchar(int c)
 {
     return serial_putchar(c);
 }
 
+__attribute__((optimize("O3")))
 int kputs(const char *s)
 {
     while (*s)
