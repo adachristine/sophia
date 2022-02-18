91ad310687ae37339db3dd5d51d47db87f2cf749
Ada Christine <adachristine18@gmail.com>
2022-02-18 17:23:40 +0000

    use the appropriate semantics for atomics

--- kc/core/cpu/irq.c
+++ kc/core/cpu/irq.c
@@ -2,21 +2,22 @@
 
 #include <stdatomic.h>
 
-static atomic_uint_fast64_t irq_lock_count = 0;
+static volatile atomic_uint_fast64_t irq_lock_count = 0;
 
 void irq_lock(void)
 {
+    irq_lock_count++;
     __asm__ volatile ("cli;");
 }
 
 void irq_unlock(void)
 {
-    if (irq_lock_count >= 1)
+    if (atomic_load(&irq_lock_count) >= 1)
     {
         irq_lock_count--;
     }
 
-    if (irq_lock_count == 0)
+    if (atomic_load(&irq_lock_count) == 0)
     {
         __asm__ volatile ("sti;");
     }
