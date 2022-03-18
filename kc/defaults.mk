850bc5ab24390abda7b0334caf3b127a5240dcd4
Ada Christine <adachristine18@gmail.com>
2022-03-18 14:35:44 +0000

    we do not need an executable stack

--- kc/defaults.mk
+++ kc/defaults.mk
@@ -5,5 +5,5 @@ CFLAGS += -fPIC -fvisibility=hidden -mgeneral-regs-only
 
 LDFLAGS += -z max-page-size=4096 --export-dynamic -pic \
 	   -z separate-code -pie -Bsymbolic -shared \
-	   -z execstack -g
+	   -z noexecstack -g
 
